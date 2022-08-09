06e00bb99a79d71ac0ac832b33578a310fef1cf6
ekamanelly <ekamanelly@gmail.com>
2022-08-10 00:55:07 +0100

    added the output files

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,18 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
+# read -p 'plases specify a tag: ' tag
+echo your container name is tag
+sudo docker build --tag emmanuelekama/tag .
 # Build image and add a descriptive tag
 
 # Step 2: 
 # List docker images
+echo listing tag on docker hub
+docker images
 
 # Step 3: 
 # Run flask app
+sudo docker run -p 8000:80 emmanuelekama/tag
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=emmanuelekama/tag
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run predictor --image=emmanuelekama/tag --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+kubectl wait --for=condition=Ready pod/predictor
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pods/predictor --address 0.0.0.0 8000:80
 
